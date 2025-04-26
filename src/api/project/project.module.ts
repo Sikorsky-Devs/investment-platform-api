3ad6a46704378e7bdba0a27fc2f3fd4cebd05f3c
thebladehit <boqdanyarmolka@gmail.com>
2025-04-26 21:17:43 +0300

    Add missed import

--- src/api/project/project.module.ts
+++ src/api/project/project.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { PrismaService } from '../../database/prisma.service';
 import { ProjectController } from './project.controller';
+import { FileModule } from '../../file/file.module';
 
 @Module({
   providers: [ProjectService, PrismaService],
   controllers: [ProjectController],
   exports: [ProjectService],
-  imports: [],
+  imports: [FileModule],
 })
 export class ProjectModule {}
